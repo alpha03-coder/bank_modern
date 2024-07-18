55cddb772aea9f8bb91d570fc1bb18154b866aaa
Ankit Kumar <anishkumar415263@gmail.com>
2024-07-18 15:19:05 +0530

    Navbar completed for mobile as well as dekstop

---
+++ src/components/index.js
@@ -0,0 +1,33 @@
+import Navbar from "./Navbar";
+import Billing from "./Billing";
+import Business from './Business';
+import CardDeal from './CardDeal';
+import Clients from './Clients';
+import CTA from './CTA';
+import Stats from './Stats';
+import Footer from './Footer';
+import Testimonials from './Testimonial';
+import Hero from './Hero';
+
+export {
+    Navbar,
+    Billing,
+    CardDeal,
+    Business,
+    Clients,
+    CTA,
+    Stats,
+    Footer,
+    Testimonials,
+    Hero,
+}
+
+
+
+
+
+
+
+
+
+
