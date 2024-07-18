659074577382881e4a3db1e8edb08e00a400e2dc
Ankit Kumar <anishkumar415263@gmail.com>
2024-07-18 18:27:22 +0530

    hero section completed and paddingX and paddingY changed

--- src/components/Getstarted.jsx
+++ src/components/Getstarted.jsx
@@ -1,8 +1,22 @@
-import React from 'react'
+import React from 'react';
+import styles from '../style';
+import { arrowUp } from '../assets'
 
 const Getstarted = () => {
   return (
-    <div>Getstarted</div>
+    <div className={`${styles.flexCenter} w-[140px] h-[140px] rounded-full bg-blue-gradient p-[2px] cursor-pointer`}>
+      <div className={`${styles.flexCenter} flex-col bg-primary w-[100%] h-[100%] rounded-full`}>
+        <div className={`${styles.flexStart} flex-row`}>
+          <p className='font-poppins text-[18px] font-medium leading-[23px] mr-2'>
+            <span className='text-gradient'>Get</span>
+          </p>
+            <img src={arrowUp} alt='arrowup' className='w-[23px] h-[23px] object-contain' />
+        </div>
+        <p className='font-poppins text-[18px] font-medium leading-[23px]'>
+          <span className='text-gradient'>Started</span>
+        </p>
+      </div>
+    </div>
   )
 }
 
