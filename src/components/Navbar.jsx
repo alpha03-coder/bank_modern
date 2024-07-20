cec7f993a3aac8691c0656e6ffcdff32758c4263
Ankit Kumar <anishkumar415263@gmail.com>
2024-07-20 08:14:21 +0530

    console removed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -33,7 +33,7 @@ const Navbar = () => {
             {navLinks.map((nav, index) => (
               <li key={nav.id} className={`font-poppins font-normal cursor-pointer text-[16px] ${index === navLinks.length - 1 ? 'mr-0' : 'mb-4'} text-white`}>
                 <a href={`#${nav.id}`}>
-                  {nav.title} {console.log(nav.id)}
+                  {nav.title}
                 </a>
               </li>
             ))}
